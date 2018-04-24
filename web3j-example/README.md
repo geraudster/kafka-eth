b105cf71181104563791c78348caafccdc4c7bcf
Geraud <geraud.dugedebernonville@zenika.com>
2018-04-24 13:23:48 +0200

    Remove spring-boot dependencies

---
+++ web3j-example/README.md
@@ -0,0 +1,8 @@
+This project reads transactions from blockchain.
+
+
+# Run
+
+```
+mvn exec:java -Dexec.args=[geth client url]
+```
--- web3j-example/src/main/java/com/zenika/kafkaeth/KafkaEthApplication.java
+++ web3j-example/src/main/java/com/zenika/kafkaeth/KafkaEthApplication.java
@@ -1,39 +1,30 @@
 package com.zenika.kafkaeth;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 import rx.Subscription;
 
 import java.util.concurrent.TimeUnit;
 
-@SpringBootApplication
 public class KafkaEthApplication {
 
-    public final static int COUNT = 10;
-    @Autowired
-    private Web3j web3j;
+    public static void main(String[] args) throws InterruptedException {
+        String ethHttpServiceUrl = args[0];
+        Web3j web3j = Web3j.build(new HttpService(ethHttpServiceUrl));
 
-    public static void main(String[] args) {
-        SpringApplication.run(KafkaEthApplication.class, args);
+        Subscription subscription = web3j.transactionObservable()
+                .doOnError(System.err::println)
+                .subscribe(tx ->
+                        System.out.println(
+                                tx.getS() + " "
+                                        + tx.getV() + " "
+                                        + tx.getFrom() + " "
+                                        + tx.getGasPrice() + " "
+                                        + tx.getTo())
+                );
+        TimeUnit.MINUTES.sleep(2);
+
+        subscription.unsubscribe();
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            Subscription subscription = web3j.transactionObservable().doOnError(System.err::println).subscribe(tx -> {
-                System.out.println(tx.getS() + " " + tx.getV() + " " + tx.getFrom() + " " + tx.getGasPrice() + " " + tx.getTo());
-            });
-            TimeUnit.MINUTES.sleep(2);
-
-            subscription.unsubscribe();
-        };
-    }
-
 }
